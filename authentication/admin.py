a843d44baacb922f5f692834596087c6517fef6c
VihOht <robsonsf@al.insper.edu.br>
2025-08-08 17:13:48 -0300

    feat: Adding user to admin page

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from authentication.models import User
+
 # Register your models here.
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["username", "email", "created_at"]
+    list_filter = ["username", "email"]
+    search_fields = ["username", "email"]
+    date_hierarchy = "created_at"
+    ordering = ["-created_at"]
