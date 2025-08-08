c003e0e9c5c89ab506884d3a4a5703f6f18e6c9f
VihOht <robsonsf@al.insper.edu.br>
2025-08-08 15:05:13 -0300

    feat: adding real numbers to "Saldo" card in main page

--- landingpage/views.py
+++ landingpage/views.py
@@ -2,13 +2,24 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render
 
+from finance_manager.models import Expenses, Incomes
+
 # Create your views here.
 
 
 @login_required
 def index(request):
     """Home page view"""
-    return render(request, "landingpage/index.html")
+    total_expenses = sum([x.amount for x in Expenses.objects.filter(user=request.user)])
+    total_incomes = sum([x.amount for x in Incomes.objects.filter(user=request.user)])
+    total_amount = total_incomes - total_expenses
+
+    context = {
+        "total_amount": f"{total_amount // 100},{total_amount % 100:02n}",
+        "well_amount": True if total_amount >= 0 else False,
+    }
+
+    return render(request, "landingpage/index.html", context)
 
 
 @login_required
